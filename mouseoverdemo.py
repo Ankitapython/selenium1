d28f770930b22a9faef842b7f0d8c1c6b6a2e013
Ankitapython <ankiitamore@gmail.com>
2019-06-16 15:43:53 +0530

    mouse hover

---
+++ mouseoverdemo.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+driver =webdriver.Chrome()
+driver.get("https://www.amazon.in/")
+driver.implicitly_wait(30)
+
+a1 = ActionChains(driver)
+cat_ele = driver.find_element_by_xpath("//span[text()='Category']")
+a1.move_to_element(cat_ele).perform()
